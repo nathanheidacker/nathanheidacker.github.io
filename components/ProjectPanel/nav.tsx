4d4b15fc40e1741e599cdc43e72b771e58b64e65
Nathan Heidacker <nathanheidacker@gmail.com>
2023-12-15 00:54:00 +0900

    bg alignment

--- components/ProjectPanel/nav.tsx
+++ components/ProjectPanel/nav.tsx
@@ -6,7 +6,7 @@ function NavButton({ direction }: { direction: "left" | "right" }) {
     const icon = direction == "left" ? "<" : ">";
     return (
         <div
-            className={`p-4 md:p-8 flex place-content-center align-middle grayscale`}
+            className={`${direction} p-4 md:p-8 flex place-content-center align-middle grayscale`}
         >
             <div className="place-self-center">{icon}</div>
         </div>
@@ -21,9 +21,7 @@ function ProjectPanelNav({ project }: { project: ProjectPanelArgs }) {
         if (styleRef.current) {
             styleRef.current.textContent = `
             .projectPanelNav > div:before {
-                background: url("${image}");
-                background-position-x: left;
-                background-position-y: center;
+                background-image: url(${image});
             }
             `;
         }
@@ -34,10 +32,46 @@ function ProjectPanelNav({ project }: { project: ProjectPanelArgs }) {
             <style ref={styleRef}></style>
             <div className="flex h-full mb-4 select-none projectPanelNav">
                 <NavButton direction="left"></NavButton>
-                <div className="grow flex place-content-center font-bold mx-4 grayscale">
+                <div className="middle grow flex place-content-center font-bold mx-4 grayscale">
                     <div className="place-self-center">{title}</div>
                 </div>
                 <NavButton direction="right"></NavButton>
+            </div>
+        </div>
+    );
+}
+
+function ProjectPanelNavs({ project }: { project: ProjectPanelArgs }) {
+    const { image, title } = project;
+    const styleRef = useRef<HTMLStyleElement>(null);
+
+    /*
+    useEffect(() => {
+        if (styleRef.current) {
+            styleRef.current.textContent = `
+            .projectPanelNav > div:before {
+                background-position-y: center;
+            }
+            `;
+        }
+    }, [project]);
+    */
+
+    return (
+        <div>
+            <style ref={styleRef}></style>
+            <div className="h-20 mb-4 flex select-none projectPanelNav">
+                <div className="hehexd">
+                    <NavButton direction="left"></NavButton>
+                </div>
+                <div className="grow mx-4">
+                    <div className="grow flex place-content-center font-bold mx-4 grayscale">
+                        <div className="place-self-center">{title}</div>
+                    </div>
+                </div>
+                <div>
+                    <NavButton direction="right"></NavButton>
+                </div>
             </div>
         </div>
     );
